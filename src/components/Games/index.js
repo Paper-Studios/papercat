8ba52522376154c2fbb57041a6d2057a8f96bacc
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-09-29 23:32:06 -0700

    Finished basic setup for games page

--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -1,6 +1,13 @@
 function Games() {
   return (
-    <div>Games Page</div>
+    <div className="games-content">
+      <div className="game">
+        <h2>Paper Perjury</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+      </div>
+    </div>
   )
 }
 export default Games;
