4ddaccd32b6d6409f5091fbef9392e6fa4cb7582
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-10-11 16:28:31 -0700

    Refactored code for presskit page

---
+++ src/components/PressKit/ImageEntry.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ImageEntry({ image }) {
+  return (
+    <img src={image} className="screenshot" alt="Property of Paper Cat Games" />
+  )
+}
+export default ImageEntry;
---
+++ src/components/PressKit/ImageList.js
@@ -0,0 +1,27 @@
+import main from '../../assets/main.jpg';
+import ss1 from '../../assets/ss1.jpg';
+import ss2 from '../../assets/ss2.jpg';
+import ss3 from '../../assets/ss3.jpg';
+import ss4 from '../../assets/ss4.png';
+import ss5 from '../../assets/ss5.png';
+
+module.export.ImageList = [
+  {
+    image: main,
+  },
+  {
+    image: ss1,
+  },
+  {
+    image: ss2,
+  },
+  {
+    image: ss3,
+  },
+  {
+    image: ss4,
+  },
+  {
+    image: ss5,
+  },
+]
