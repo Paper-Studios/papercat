5c99b30287891daae1d0c794c89c7376e97ade66
nsadakuni <narusadakuni1@gmail.com>
2023-10-11 11:41:03 -0700

    mobile css for large and x-small

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -11,7 +11,7 @@ function Team() {
         Meet Our Team
       </h1>
       <div className='member'>
-        <img src='/assets/logo.png' height='200%' width='200%'/>
+        <img className='member-photo' src='/assets/logo.png' height='200%' width='200%'/>
         <div>
           <h2>
             John Doe
@@ -19,13 +19,13 @@ function Team() {
           <h4>
             CEO
           </h4>
-          <p>
+          <p className='member-blurb'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
       </div>
       <div className='member'>
-        <img src='/assets/logo.png' height='200%' width='200%'/>
+        <img className='member-photo' src='/assets/logo.png' height='200%' width='200%'/>
         <div>
           <h2>
             Jane Doe
@@ -33,7 +33,7 @@ function Team() {
           <h4>
             Software Engineer
           </h4>
-          <p>
+          <p className='member-blurb'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
