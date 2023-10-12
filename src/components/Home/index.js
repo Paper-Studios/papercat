4b94996667167c116ef439ef9dc2ea0f091dc486
Rachel Miller <rkmiller131@gmail.com>
2023-10-12 09:15:47 -0700

    Added lifted paper effect to blurb titles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -14,16 +14,16 @@ function Home() {
           />
       </div>
       <div className='blurb'>
-        Solve Crimes, Outsmart Criminals
+        <span>Solve Crimes, Outsmart Criminals</span>
       </div>
       <div className='home-content'>
         <div className='home-description'>
           <p id='abstract'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <p id='release'>
-            Coming Early 2024
-          </p>
+          <div className='blurb release'>
+            <span>Coming Early 2024</span>
+          </div>
         </div>
         <div className='home-screenshots'>
           <img src='/assets/screenshot1.jpg' height='45%' width='80%' alt='gameplay'/>
