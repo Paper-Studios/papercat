b0c1d6c46c6609158640d81315485dce841b3de1
Rachel Miller <rkmiller131@gmail.com>
2023-10-23 08:01:38 -0700

    Added styling to team page with member cards and font update

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -11,22 +11,34 @@ function Team() {
         Meet Our Team
       </h1>
       <div className='member'>
-        <img className='member-photo' src='/assets/logo.png' height='200%' width='200%'/>
-        <div>
+        <div className='member-image'>
+          <div className='member-card' />
+          <div className='member-skewed-card' />
+          <div className='member-image-holder'>
+            <img className='member-photo' src='/assets/logo.png' height='200%' width='200%' alt='temp'/>
+          </div>
+        </div>
+        <div className='member-content'>
           <h2>
-            John Doe
+            John Smith
           </h2>
           <h4>
-            CEO
+            Game Developer
           </h4>
           <p className='member-blurb'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
           </p>
         </div>
       </div>
       <div className='member'>
-        <img className='member-photo' src='/assets/logo.png' height='200%' width='200%'/>
-        <div>
+        <div className='member-image'>
+          <div className='member-card' />
+          <div className='member-skewed-card' />
+          <div className='member-image-holder'>
+            <img className='member-photo' src='/assets/logo.png' height='200%' width='200%' alt='temp'/>
+          </div>
+        </div>
+        <div className='member-content'>
           <h2>
             Jane Doe
           </h2>
@@ -34,7 +46,7 @@ function Team() {
             Software Engineer
           </h4>
           <p className='member-blurb'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
       </div>
