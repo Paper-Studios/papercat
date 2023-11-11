cb565b4779dc26d1b11073c5b3ca0db781223c97
Dingo <ethan.pinsker@gmail.com>
2023-11-11 10:50:42 -0800

    added contact boxes/spacing

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,39 @@
+import React from 'react';
+
 function Contact() {
   return (
-    <div>Contact Page poop</div>
+    <div className='Contact'>
+
+      <div className='contactGreeter'>Contact Us</div>
+
+      <div className='contactMain'>
+
+        <div className= 'contactBackdrop'>
+          <div className='contactImage'>
+            <img src={'assets/standingJustina.png'} id='stndJustina' width='40%' alt='Contact Image'/>
+          </div>
+        </div>  
+
+        <div className='contactBoxes'>
+          <label>Email</label>
+
+            <input className='textBackdrop' type="text" id="email"/>
+
+          <label>Message</label>
+
+            <input className='textBackdrop' type="text" id="mail"/>
+
+          <button onclick="myFunction()">Send</button> 
+        </div>
+
+      </div>
+
+    </div>
   )
 }
+
+function myFunction() {
+  alert("Sent!");
+}
+
 export default Contact;
