bc5c501eafb951c92e973870bf6d8d3216906f87
Dingo <ethan.pinsker@gmail.com>
2023-12-12 15:57:22 -0800

    contact page done

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,29 +1,32 @@
 import React from 'react';
+import Button from '../Shared/Button.jsx';
 
 function Contact() {
   return (
     <div className='Contact'>
 
-      <div className='contactGreeter'>Contact Us</div>
-
+      
       <div className='contactMain'>
 
         <div className= 'contactBackdrop'>
           <div className='contactImage'>
-            <img src={'assets/standingJustina.png'} id='stndJustina' width='40%' alt='Contact Image'/>
+            <img src={'assets/ContactUs.png'} id='stndJustina' width='40%' alt='Contact Image'/>
           </div>
         </div>  
 
         <div className='contactBoxes'>
-          <label>Email</label>
-
-            <input className='textBackdrop' type="text" id="email"/>
-
-          <label>Message</label>
-
-            <input className='textBackdrop' type="text" id="mail"/>
-
-          <button onclick="myFunction()">Send</button> 
+          
+          <div className='contactInputs'>
+            <label>Name</label>
+              <input className='textBackdrop' type="text" id="name"/>
+            <label>Email</label>
+              <input className='textBackdrop' type="text" id="email"/>
+            <label>Message</label>
+              <textarea className='textBackdrop' id="mail" name="mail"></textarea>
+          </div>
+          
+          <Button icon={'send'} text={'Send'} href={'/presskit'} type={'button'}/>
+        
         </div>
 
       </div>
